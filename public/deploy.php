11038fb50a80eacb8eb87ac67b859abef0034305
kris <kris@krisdeMacBook-Pro.local>
2018-05-21 15:40:55 +0800

    init
    init

--- public/deploy.php
+++ public/deploy.php
@@ -1,31 +1,22 @@
 <?php
 
-// 本地仓库路径
-$local = '/mnt/www/junan.viirose.com';
+$path = "/mnt/www/junan.viirose.com";
+$key = 'vlQUTCw7EguCqY5F';
 
-// 安全验证字符串，为空则不验证
-$token = 'vlQUTCw7EguCqY5F';
-
-
-// 如果启用验证，并且验证失败，返回错误
-$httpToken = isset($_SERVER['HTTP_X_GITLAB_TOKEN']) ? $_SERVER['HTTP_X_GITLAB_TOKEN'] : '';
-if ($token && $httpToken != $token) {
-    header('HTTP/1.1 403 Permission Denied');
-    die('Permission denied.');
-}
-
-// 如果仓库目录不存在，返回错误
-if (!is_dir($local)) {
-    header('HTTP/1.1 500 Internal Server Error');
-    die('Local directory is missing');
-}
-
-//如果请求体内容为空，返回错误
+$github_signature = @$_SERVER['HTTP_X_HUB_SIGNATURE'];
 $payload = file_get_contents('php://input');
-if (!$payload) {
-    header('HTTP/1.1 400 Bad Request');
-    die('HTTP HEADER or POST is missing.');
-}
 
-echo shell_exec("cd {$local} && git pull 2>&1");
-die("done " . date('Y-m-d H:i:s', time()));
+$arr = explode('=', $github_signature);
+$algo = $arr[0];
+$signature = $arr[1];
+
+$payload_hash = hash_hmac($algo, $payload, $key);
+if($payload_hash == $signature){
+    shell_exec('cd '.$path);
+    shell_exec('/usr/bin/git pull');
+    shell_exec('chown -R nginx:nginx'.$path);
+    return 200;
+}else{
+   return 'invalid key!'; 
+} 
+?>
