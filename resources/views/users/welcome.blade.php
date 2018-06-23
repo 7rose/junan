67b667e12494eac505b614ee103851a2ae4dc5a7
kris <kris@krisdeMacBook-Pro.local>
2018-06-23 21:06:10 +0800

    init
    init

--- resources/views/users/welcome.blade.php
+++ resources/views/users/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 <div class="welcome">
     <img class="welcome-img" src="{{ URL::asset('junan/images/junan.svg') }}">
-    <h3>军安集团综合管理系统用户说明</h3>
+    <h3>军安集团综合管理系统</h3>
     <span>2018&nbsp&copy&nbsp江苏军安驾培集团</span>
 </div>
   
