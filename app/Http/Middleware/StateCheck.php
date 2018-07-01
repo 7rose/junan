c894a51eb9d5e7d3427132b6f713913ed7611727
kris <kris@krisdeMacBook-Pro.local>
2018-07-01 11:02:57 +0800

    init
    init

--- app/Http/Middleware/StateCheck.php
+++ app/Http/Middleware/StateCheck.php
@@ -20,18 +20,14 @@ class StateCheck
     public function handle($request, Closure $next)
     {
         $record = User::leftJoin('branches', 'users.branch', '=', 'branches.id')
-                        ->select('users.locked', 'branches.show as branch_show')
+                        ->select('users.locked', 'users.new', 'branches.show as branch_show')
                         ->find(Session::get('id'));
 
         $auth = new Auth;
-        if($record->locked || !$auth->user() || !$record->branch_show){
-            return redirect('/locked');
-        }else{
-            if($record->new && Request::path() != 'user/reset_password' && Request::path() != 'user/update_password'){
-                return redirect('/user/reset_password');
-            }else{
-                return $next($request);
-            }
-        }
+        if($record->locked || !$record->branch_show) return redirect('/locked');
+        if(!$auth->user()) return redirect('/locked');
+        if($record->new && Request::path() != 'user/reset_password' && Request::path() != 'user/update_password') return redirect('/user/reset_password');
+        
+        return $next($request);
     }
 }
