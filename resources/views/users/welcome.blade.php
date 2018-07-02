a5d5014412328f94b531820cfa9c931a11ec5274
kris <kris@krisdeMacBook-Pro.local>
2018-07-02 16:55:38 +0800

    init
    init

--- resources/views/users/welcome.blade.php
+++ resources/views/users/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <div class="welcome">
-    <img class="welcome-img" src="{{ URL::asset('images/'.Config::get('ginkgo')['logo'].'.svg') }}">
+    <img class="welcome-img" src="{{ URL::asset('images/junan.svg') }}">
     <h3>{{ Config::get('ginkgo')['name'] }}管理系统</h3>
     <span>2018&nbsp&copy&nbsp{{ Config::get('ginkgo')['copy_right'] }}</span>
 </div>
