8741685bf541637068fbdbc714306040a5c5be7e
kris <kris@krisdeMacBook-Pro.local>
2018-07-19 01:57:14 +0800

    init
    init

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,10 @@
+@extends('../nav')
+
+@section('container')
+    <div class="col-md-6 col-md-offset-3">
+       <div style="text-align: center" class="alert alert-danger">
+        <h1><span class="glyphicon glyphicon-warning-sign"></span></h1>
+            503: 服务器没有响应当前操作; 请联系系统管理员, 或直接发送邮件至技术服务:<email>hi@viiroes.com</email>
+        </div>
+    <div class="col-md-6 col-md-offset-3">
+@endsection
