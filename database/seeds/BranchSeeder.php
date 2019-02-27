2a630d8e010a1d072b941b6b0b00b838e99b46c7
kris <kris@viirose.com>
2019-02-27 12:55:29 +0800

    init
    init

--- database/seeds/BranchSeeder.php
+++ database/seeds/BranchSeeder.php
@@ -14,43 +14,22 @@ class BranchSeeder extends Seeder
         DB::table('branches')->insert([
             [
                 'parent_id'=> 0,
-                'text'=>"军安集团",
+                'text'=>"银杏集团",
              ],[
                 'parent_id'=> 1,
-                'text'=>"军安(马厂)",
+                'text'=>"银安",
              ],[
                 'parent_id'=> 1,
-                'text'=>"恒通(沭城)",
+                'text'=>"恒通",
              ],[
                 'parent_id'=> 1,
-                'text'=>"军越(汤涧)",
+                'text'=>"银越",
              ],[
                 'parent_id'=> 1,
-                'text'=>"鸿远(沭城)",
+                'text'=>"鸿远",
              ],[
                 'parent_id'=> 1,
-                'text'=>"军兴(周集)",
-             ],[
-                'parent_id'=> 1,
-                'text'=>"军顺(沭城)",
-             ],[
-                'parent_id'=> 1,
-                'text'=>"军畅(耿圩)",
-             ],[
-                'parent_id'=> 1,
-                'text'=>"永安(沭城)",
-             ],[
-                'parent_id'=> 1,
-                'text'=>"军盛(沂涛)",
-             ],[
-                'parent_id'=> 1,
-                'text'=>"元顺(塘沟)",
-             ],[
-                'parent_id'=> 1,
-                'text'=>"元安(章集)",
-             ],[
-                'parent_id'=> 1,
-                'text'=>"杰达(北丁集)",
+                'text'=>"银兴",
              ],
         ]);
     }
